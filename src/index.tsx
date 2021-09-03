335eb13ec33c9aa681f5c36decdef7a182ba57a1
Mose Schmiedel <mose@schmiednet.de>
2021-09-03 13:35:32 +0200

    Changes to integrate into Coocook platform

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,25 @@ import ReactDOM from "react-dom";
 import IngredientsEditor from "./components/IngredientsEditor";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const divArguments = {
-    projectId: 1,
-    dishId: 1,
+import type { ProjectDef } from './util/Definitions';
+
+const project: ProjectDef = {
+    // @ts-ignore
+    type: ingredientsEditorData.dish_id ? 'dish' : 'recipe',
+    // @ts-ignore
+    id: ingredientsEditorData.project_id,
+    // @ts-ignore
+    name: ingredientsEditorData.project_name,
+    // @ts-ignore
+    specificId: ingredientsEditorData.dish_id || ingredientsEditorData.recipe_id,
 };
 
 ReactDOM.render(
     <React.StrictMode>
-        <IngredientsEditor {...divArguments} />
+        <IngredientsEditor project={project} />
     </React.StrictMode>,
 
-    document.getElementById("root")
+    document.getElementById("ingredients-editor")
 );
 
 if (import.meta.hot) {
